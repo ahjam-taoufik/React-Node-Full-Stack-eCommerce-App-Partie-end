5ac91a129ef4c507f912e1f237e3061d21b17bd0
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-15 20:00:23 +0100

    add filter

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+
+import Product from "./pages/Product";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
+import { Route, Routes } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<ProductList/>} />
+        <Route path="/products/:category" element={<ProductList/>} />
+        <Route path="/product" element={<Product/>} />
+        <Route path="/product/:id" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+
+       
+    </Routes>
+      //  <div>
+      //    <Home/>
+      //    <ProductList/>
+      //    <Product/>
+      //    <Login/>
+      //    <Register/>
+      //    <Cart/>
+      //  </div>
+  );
+}
+
+export default App;
